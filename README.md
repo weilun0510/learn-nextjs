d8cd0331667e0efd3479ec8d883ee0e2ba15d61a
weilun0510 <13566106399@163.com>
2024-09-10 10:47:49 +0800

    delete invoice

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ learn nextJs 14 + app router
 - https://nextjs.net.cn/learn?utm_source=next-site&utm_medium=navbar
 
 visit: https://www.images798.top/
+
+- Zod 验证类型
+- zustand 状态管理
--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -16,6 +16,7 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
@@ -31,6 +32,31 @@ export async function createInvoice(formData: FormData) {
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
 
+  // 调用 revalidatePath 以清除客户端缓存并发出新的服务器请求。
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
